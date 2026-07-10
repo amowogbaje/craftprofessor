3df0106e9649c8889a175b15ea74ffa892cfd7ee
Gideon Amowogbaje <amowogbajegideon@gmail.com>
2026-07-10 03:41:08 +0100

    removing the space

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
     'allowed_methods' => ['*'],
 
     // Comma-separated in .env, e.g. CORS_ALLOWED_ORIGINS=http://localhost:5173,https://app.yourdomain.com
-    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://localhost:5174, https://craftprofessorui.amowogbaje.com'))),
+    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://localhost:5174,https://craftprofessorui.amowogbaje.com'))),
 
     'allowed_origins_patterns' => [],
 
